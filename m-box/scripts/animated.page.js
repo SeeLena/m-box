1908e72d391bab685788d18b3acb015a018f5ee4
SeeLena <86488928+SeeLena@users.noreply.github.com>
2021-10-28 00:55:26 +0300

    M-box

---
+++ m-box/scripts/animated.page.js
@@ -0,0 +1,87 @@
+
+jQuery(document).ready(function() {
+    jQuery('.banner').addClass("hidden").viewportChecker({
+        classToAdd: 'text-focus-in',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.poster').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.description').addClass("hidden").viewportChecker({
+        classToAdd: 'text-focus-in',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.trending').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.generes').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.generes__boxes_wrapper').addClass("hidden").viewportChecker({
+        classToAdd: 'text-focus-in',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.actors').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.banner-soon').addClass("hidden").viewportChecker({
+        classToAdd: 'text-focus-in',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.banner-register').addClass("hidden").viewportChecker({
+        classToAdd: 'text-focus-in',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.featured').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.foundation').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.awards').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.get-started').addClass("hidden").viewportChecker({
+        classToAdd: 'slide-in-left',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+
+    jQuery('.app__item').addClass("hidden").viewportChecker({
+        classToAdd: 'flip-in-ver-right',
+        offset: 400,
+        classToRemove: 'hidden'
+    });
+    
+});
